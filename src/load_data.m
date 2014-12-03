4527c7b90b8456a9e09d460a210249f33a6e783f
Gernot Vormayr <gvormayr@gmail.com>
2014-12-03 11:38:28 +0100

    Added missing rx changes to load_data

--- src/load_data.m
+++ src/load_data.m
@@ -1,6 +1,12 @@
-function v = load_data(filename, count)
+function v = load_data(filename, count, wire)
+    switch wire
+        case 8
+            wire = 'int8';
+        case 16
+            wire = 'int16';
+    end
     f = fopen(filename, 'rb');
-    t = fread(f, [2, count], 'schar');
+    t = fread(f, [2, count], wire);
     v = t(1,:) + t(2,:)*1i;
     [r, c] = size(v);
     v = reshape(v, c, r);
