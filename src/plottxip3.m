f5f05d72c8bd325014c1772d3a064ec2a076752e
Gernot Vormayr <gvormayr@gmail.com>
2015-02-14 18:46:49 +0100

    final tx ip3 export

--- src/plottxip3.m
+++ src/plottxip3.m
@@ -1,15 +1,15 @@
-load('29-Jan-2015/tx_ip3.mat');
+load('13-Feb-2015/caltx_ip3.mat');
 
-X = 20*log10(res(1,:,1));
-La = log10(mean(10.^res(:,:,2))) - gain;
-LIM3 = log10(mean(10.^res(:,:,4)));
+corr = pow2db(mean(db2pow(res(1,15,:) - gain))/2);
+cable = dlmread('13-Feb-2015/kabel16_13feb.s2p', '\t', 5, 0);
+cable = mag2db(abs(cable(33,6)+1j*cable(33,7)));
 
-load('29-Jan-2015/caltx_ip3.mat');
-realLa = log10(mean(reshape(mean(10.^res, 3), 50, 20))) - 10*log10(2) - gain;
-mErr = La - realLa;
-LIM3 = LIM3 - mErr;
-La = realLa;
+load('13-Feb-2015/tx_ip3.mat');
+corr = corr - (res(1,15,2) - gain);
 
+X = mag2db(0.5*res(1,:,1));
+La = pow2db(mean(db2pow(res(:,:,2)- gain - corr - cable)));
+LIM3 = pow2db(mean(db2pow(res(:,:,4) - gain - corr - cable)));
 
 basedir = '../tex/data/ip3/tx/';
 
@@ -19,9 +19,9 @@ dlmwrite(sprintf('%sla', basedir), [X' La'], 'delimiter', '\t');
 dlmwrite(sprintf('%sim3', basedir), [X' LIM3'], 'delimiter', '\t');
 
 pLa = polyfit(X(1:end), La(1:end), 1)
-pLIM3 = polyfit(X(11:end), LIM3(11:end), 1)
+pLIM3 = polyfit(X(12:end), LIM3(12:end), 1)
 
-pX = linspace(-20, 15);
+pX = linspace(-30, 20);
 
 yLa = polyval(pLa, pX);
 yLIM3 = polyval(pLIM3, pX);
